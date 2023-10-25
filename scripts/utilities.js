8ed26cb29b54450c10c0319229f7234ee7783310
pkuczak <paulkuczak@gmail.com>
2023-10-25 02:02:09 -0400

    added breaks to create whitespace

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -9,21 +9,22 @@ const reset = () => {
 export function displayWordInfo(wordInfo, inputWord) {
     let wordDiv = document.createElement("div");
     wordDiv.class = "word";
-    wordDiv.innerHTML = `<h3>${inputWord}</h3>`;
+    wordDiv.innerHTML = `<br><h3>${inputWord}</h3>`;
 
     let infoDiv = document.createElement("div");
     infoDiv.class = "info";
     infoDiv.innerHTML = `
     <p>${wordInfo[0].meanings[0].partOfSpeech}</p>
-    <p>/${wordInfo[0].phonetic}/</p>`;
+    <p>/${wordInfo[0].phonetic}/</p>
+    <br>`;
 
     let definePara = document.createElement("p");
     definePara.class = "word-meaning";
-    definePara.textContent = `${wordInfo[0].meanings[0].definitions[0].definition}`;
+    definePara.textContent = `Definition: ${wordInfo[0].meanings[0].definitions[0].definition}`;
 
     let examplePara = document.createElement("p")
     examplePara.class = "word-example";
-    examplePara.textContent = `${wordInfo[0].meanings[0].definitions[0].example}`;
+    examplePara.textContent = `Example: ${wordInfo[0].meanings[0].definitions[0].example}`;
 
     reset();
 
