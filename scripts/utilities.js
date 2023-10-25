0af18c9f4d00a720782f0f205a79884cdb441917
pkuczak <paulkuczak@gmail.com>
2023-10-25 17:23:49 -0400

    broke apart display function, added if/else

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -7,30 +7,72 @@ const reset = () => {
 
 // Build and Display the parts of the dictionary
 export function displayWordInfo(wordInfo, inputWord) {
-    let wordDiv = document.createElement("div");
-    wordDiv.class = "word";
-    wordDiv.innerHTML = `<br><h3>${inputWord}</h3>`;
-
-    let infoDiv = document.createElement("div");
-    infoDiv.class = "info";
-    infoDiv.innerHTML = `
-    <p>${wordInfo[0].meanings[0].partOfSpeech}</p>
-    <p>/${wordInfo[0].phonetic}/</p>
-    <br>`;
-
-    let definePara = document.createElement("p");
-    definePara.class = "word-meaning";
-    definePara.textContent = `Definition: ${wordInfo[0].meanings[0].definitions[0].definition}`;
-
-    let examplePara = document.createElement("p")
-    examplePara.class = "word-example";
-    examplePara.textContent = `Example: ${wordInfo[0].meanings[0].definitions[0].example}`;
-
     reset();
 
-    result.appendChild(wordDiv);
-    result.appendChild(infoDiv);
-    result.appendChild(definePara);
-    result.appendChild(examplePara);
+    result.appendChild(createWordTitle(inputWord));
+    result.appendChild(createWordDetails(wordInfo));
+    result.appendChild(createDefinition(wordInfo));
+    result.appendChild(createExample(wordInfo));
 };
 
+export function displayError() {
+    let errorMessage = "Couldn't Find The Word";
+    result.innerHTML = `<br><h3 class="error">${errorMessage}</h3>`;
+};
+
+function createExample(wordInfo) {
+    let examplePara = document.createElement("p")
+    examplePara.class = "word-example";
+    let exampleSentence;
+    if (wordInfo[0].meanings[0].definitions[0].example) {
+        exampleSentence = wordInfo[0].meanings[0].definitions[0].example;
+    } else {
+        exampleSentence = "No example found.";
+    };
+    examplePara.textContent = `Example: ${exampleSentence}`;
+    return examplePara;
+};
+
+function createDefinition(wordInfo) {
+    let definePara = document.createElement("p");
+    definePara.class = "word-meaning";
+    let definitionSentence;
+    if (wordInfo[0].meanings[0].definitions[0].definition) {
+        definitionSentence = wordInfo[0].meanings[0].definitions[0].definition;
+    } else {
+        definitionSentence = "No definition found.";
+    };
+    definePara.textContent = `Definition: ${definitionSentence}`;
+    return definePara;
+};
+
+function createWordDetails(wordInfo) {
+    let infoDiv = document.createElement("div");
+    infoDiv.class = "info";
+    let wordType;
+    if (wordInfo[0].meanings[0].partOfSpeech) {
+        wordType = wordInfo[0].meanings[0].partOfSpeech;
+    } else {
+        wordType = "No part of speech found.";
+    };
+
+    let wordPronounce;
+    if (wordInfo[0].phonetic) {
+        wordPronounce = `/${wordInfo[0].phonetic}/`;
+    } else {
+        wordPronounce = "No phonetics found.";
+    };
+
+    infoDiv.innerHTML = `
+    <p>${wordType}</p>
+    <p>${wordPronounce}</p>
+    <br>`;
+    return infoDiv;
+};
+
+function createWordTitle(word) {
+    let wordDiv = document.createElement("div");
+    wordDiv.class = "word";
+    wordDiv.innerHTML = `<br><h3>${word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()}</h3>`;
+    return wordDiv;
+}
